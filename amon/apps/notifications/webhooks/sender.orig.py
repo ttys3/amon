5ec8e40274dcca9b325cb254a2972f21711d03a1
荒野無燈 <i@80x86.io>
2019-07-01 18:38:02 +0800

    webhook notification: mod for dingtalk robot

--- amon/apps/notifications/webhooks/sender.orig.py
+++ amon/apps/notifications/webhooks/sender.orig.py
@@ -1,17 +1,9 @@
 import logging
 import requests
 import json
-from dingtalkchatbot.chatbot import DingtalkChatbot
 
 logger = logging.getLogger(__name__)
 
-'''
-xiaoding.send_markdown(title='amon警报', text='#### Server:web1 Disk<80% for 30.0 minutes  (Current value: vda1/24.71%)\n'
-                           '> ###### server: {"name": "web1"}\n\n'
-                           '> ###### trigger: {"from": 13988886666, "average_value": 24.71, "to": 13988886666}\n\n'
-                           '> ###### alert: {"key": null, "above_below": "below", "rule_type": "system", "period": 1800, "metric": "Disk", "metric_value": 80, "metric_type": "%"}\n\n',
-                           is_at_all=True)
-'''
 
 def _send_webhook(auth=None, data=None):
     url = auth.get('url')
@@ -19,27 +11,19 @@ def _send_webhook(auth=None, data=None):
 
     if secret:
         data['secret'] = secret
-
-    #data = json.dumps(data)
-    xiaoding = DingtalkChatbot(url)
+    
+    data = json.dumps(data)
+    
     error = None
     try:
-        #r = requests.post(url, data=data, timeout=5)
-        if 'server' not in data:
-            print("sending tests ...")
-            xiaoding.send_text(msg=json.dumps(data), is_at_all=False)
-        else:
-            print("sending notification ...")
-            xiaoding.send_markdown(title= 'amon警报: %s' %(data['message']), text='#### {message} \n'
-                                       '> ###### server: {server_name}\n\n'
-                                       '> ###### trigger: {trigger}\n\n'
-                                       '> ###### alert: {alert}\n\n'.format(message=data['message'], server_name=data['server']['name'], trigger=data['trigger'], alert=data['alert']),
-                                       is_at_all=True)
+        r = requests.post(url, data=data, timeout=5)
     except Exception as e:
         logger.exception('Can not send webhook: {0}'.format(e))
         error = e
 
+
     return error
+        
 
 def generate_webhook_data(notification=None, message=None):
     hook_values = {
